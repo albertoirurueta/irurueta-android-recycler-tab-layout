21ac4ceb369df943d35422a452511c61e757a4cb
Alberto Irurueta <alberto@irurueta.com>
2021-10-09 13:13:24 +0200

    Fixed tests

--- app/src/androidTest/java/com/irurueta/android/recyclertablayout/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/irurueta/android/recyclertablayout/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.irurueta.android.recyclertablayout", appContext.packageName)
+        assertEquals("com.irurueta.android.recyclertablayout.app", appContext.packageName)
     }
 }
